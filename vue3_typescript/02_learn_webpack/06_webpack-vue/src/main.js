9d255625e4743118e22dc13c8a71638b0f410b85
nulideaqian <justdoalitter@gmail.com>
2022-05-28 01:27:42 +0800

    chpater08 1:10

--- vue3_typescript/02_learn_webpack/06_webpack-vue/src/main.js
+++ vue3_typescript/02_learn_webpack/06_webpack-vue/src/main.js
@@ -1,9 +1,11 @@
 import {sum} from './js/math'
+
 const {priceFormat} = require('./js/format')
 import './js/element'
 import './css/style.css'
+import {createApp} from 'vue/dist/vue.runtime.esm-bundler'
 
-console.log(sum(20,30))
+console.log(sum(20, 30))
 console.log(priceFormat())
 
 const message = "Hello World"
@@ -11,3 +13,12 @@ const names = ['abc', 'cba', 'nba']
 
 names.forEach(item => console.log(item))
 console.log(message)
+
+createApp({
+  template: '<h2>Hello World</h2>',
+  data() {
+    return {
+      title: 'Hello World'
+    }
+  }
+}).mount('#app')
