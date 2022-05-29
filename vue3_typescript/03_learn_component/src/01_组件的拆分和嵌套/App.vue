85a7a5a280d10fc7db6f30a2df9b26b72944c661
nulideaqian <justdoalitter@gmail.com>
2022-05-30 03:23:02 +0800

    chapter11 36min

---
+++ vue3_typescript/03_learn_component/src/01_组件的拆分和嵌套/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Main from './Main.vue'
+import Footer from './Footer.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue3_typescript/03_learn_component/src/01_组件的拆分和嵌套/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <MainBanner />
+    <MainProductList />
+  </div>
+</template>
+
+<script>
+import MainBanner from './MainBanner.vue'
+import MainProductList from './MainProductList.vue'
+
+export default {
+  components: {
+    MainBanner,
+    MainProductList
+  }
+}
+</script>
+
+<style scoped>
+</style>
