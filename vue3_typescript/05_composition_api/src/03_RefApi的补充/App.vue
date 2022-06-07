8e900f42c71e09c43ce04b00262cec43c1d26cae
nulideaqian <justdoalitter@gmail.com>
2022-06-08 00:58:47 +0800

    chapter16 58min

---
+++ vue3_typescript/05_composition_api/src/03_RefApi的补充/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="message">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+import debounceRef from './hook/useDebounceRef'
+
+export default {
+  setup() {
+    const message = debounceRef('Hello World')
+
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue3_typescript/05_composition_api/src/main.js
+++ vue3_typescript/05_composition_api/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 // import App from './01_mixin和extends/App'
-import App from './02_compositionAPI基础使用/App'
+// import App from './02_compositionAPI基础使用/App'
+// import App from './03_RefApi的补充/App'
+import App from './04_computed和watch/App'
 
 createApp(App).mount('#app')
